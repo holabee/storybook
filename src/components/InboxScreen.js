d0e949c58aee18ea3c8ef0c20aa9ed610f54c19d
holabee <function.hola.b@gmail.com>
2021-01-07 10:18:48 +0900

    taskbox ui

---
+++ src/components/InboxScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import PureTaskList from './TaskList';
+
+export function PureInboxScreen({ error }) {
+  if (error) {
+    return (
+      <div className="page lists-show">
+        <div className="wrapper-message">
+          <span className="icon-face-sad" />
+          <div className="title-message">oh no!</div>
+          <div className="subtitle-message">something went wrong</div>
+        </div>
+      </div>
+    )
+  }
+  
+  return (
+    <div className="page lists-show">
+      <nav>
+        <h1 className="title-page">
+          <span className="title-wrapper">Taskbox</span>
+        </h1>
+      </nav>
+      <TaskList />
+    </div>
+  );
+}
+
+PureInboxScreen.propTypes = {
+  error: PropTypes.string,
+};
+
+PureInboxScreen.defaultProps = {
+  error: null,
+};
+
+export default connect(({ error }) => ({ error }))(PureInboxScreen);
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 
 import Task from './Task';
 import { connect } from 'react-redux';
-import { archiveTask, pinTask } from '../lib/redux';
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+import redux, { archiveTask, pinTask } from '../lib/redux';
+
+export default function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -54,13 +55,24 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     </div>
   );
 }
-TaskList.propTypes = {
+
+PureTaskList.propTypes = {
   loading: PropTypes.bool,
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
   onPinTask: PropTypes.func.isRequired,
   onArchiveTask: PropTypes.func.isRequired,
 };
 
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false,
 };
+
+// export default connect(
+//   ({ tasks }) => ({
+//     tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'),
+//   }),
+//   dispatch => ({
+//     onArchiveTask: id => dispatch(archiveTask(id)),
+//     onPinTask: id => dispatch(pinTask(id)),
+//   })
+// )(PureTaskList);
